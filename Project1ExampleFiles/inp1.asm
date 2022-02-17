ba408af0db1fa424ce7656bf7d8472ba2e3c015a
Dee <greend40@uwosh.edu>
2022-02-17 11:30:32 -0600

    files added

---
+++ Project1ExampleFiles/inp1.asm
@@ -0,0 +1,4 @@
+0: RDD R0
+2: RDD R1
+4: ADD R0 R1 R2
+6: PRT R2
---
+++ microputer.c
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Description: Program for reading in a binary program file as input, 
+ * translating the input code to assembly code and outputting the resulting to 
+ * a text file. 
+ * Author: Demetrios Green
+ * Date: 02-08-2022
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+ return 0;
+}
+
+void decode_instruction(char instruction[],
+                        char* op,
+                        char* ri,
+                        char* rj,
+                        char* rk,
+                        short* imm,
+                        char* addr)
+{
+
+
+
+
+
+}
+
+int execute_program(char program[])
+{
+	
+}
