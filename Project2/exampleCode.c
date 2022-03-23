b0b51a0c9a02239f035f8aa9db6e6b4117e67b37
Demetrios Green <greend40@uwosh.edu>
2022-03-22 19:36:01 -0500

    Unified formatting to use tabs(2), spacing, and next-line bracketing.

--- Project2/exampleCode.c
+++ Project2/exampleCode.c
@@ -2,10 +2,10 @@
  * Description: Original sample code provided with lab
  * Author: Demetrios Green & Ben Pink
  * Date: 03-30-2022
-*******************************************************************************/
+ *******************************************************************************/
 
 #include <string.h>
-#include <unistd.h> // pipe/fork/dup2
+#include <unistd.h>		// pipe/fork/dup2
 #include <sys/wait.h> // waitpid
 
 const int READ = 0;
@@ -18,43 +18,43 @@ pipe(pipes[1]);
 pid = fork();
 if (pid == 0)
 {
-// Left child writes into WRITE end of first pipe:
-dup2(pipes[0][WRITE], STDOUT_FILENO);
-for (i = 0; i < NUM_PIPES; i++) // Close each end of all pipes
-{
-close(pipes[i][READ]);
-close(pipes[i][WRITE]);
-}
-// call exec* ??
-}
-else
-{
-pid = fork();
-if (pid == 0)
-{
-// Middle child reads from READ end of first pipe
-// Middle child writes to WRITE end of second pipe
-// Close each end of all pipes
-// call exec*
+	// Left child writes into WRITE end of first pipe:
+	dup2(pipes[0][WRITE], STDOUT_FILENO);
+	for (i = 0; i < NUM_PIPES; i++) // Close each end of all pipes
+	{
+		close(pipes[i][READ]);
+		close(pipes[i][WRITE]);
+	}
+	// call exec* ??
 }
 else
 {
-pid = fork();
-if (pid == 0)
-{
-// Right child reads from READ end of second pipe:
-dup2(pipes[1][READ], STDIN_FILENO);
-// Maybe duplicate STDOUT as an arbitrary file?
-// dup2(??, STDOUT_FILENO);
-// Close each end of all pipes
-// call exec*
-}
-else
-{
-// Parent
-// Close each end of all pipes
-// Wait until the last child terminates:
-waitpid(pid, &status, 0); // Or, maybe don’t wait?
-}
-}
+	pid = fork();
+	if (pid == 0)
+	{
+		// Middle child reads from READ end of first pipe
+		// Middle child writes to WRITE end of second pipe
+		// Close each end of all pipes
+		// call exec*
+	}
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			// Right child reads from READ end of second pipe:
+			dup2(pipes[1][READ], STDIN_FILENO);
+			// Maybe duplicate STDOUT as an arbitrary file?
+			// dup2(??, STDOUT_FILENO);
+			// Close each end of all pipes
+			// call exec*
+		}
+		else
+		{
+			// Parent
+			// Close each end of all pipes
+			// Wait until the last child terminates:
+			waitpid(pid, &status, 0); // Or, maybe don’t wait?
+		}
+	}
 }
--- Project2/exampleCode2.c
+++ Project2/exampleCode2.c
@@ -2,79 +2,82 @@
  * Description: Updated sample code provided in class
  * Author: Demetrios Green & Ben Pink
  * Date: 03-30-2022
-*******************************************************************************/
+ *******************************************************************************/
 
 #include <stdio.h>
-#include <unistd.h> // pipe/fork/dup2
+#include <unistd.h>		// pipe/fork/dup2
 #include <sys/wait.h> // waitpid
+
 const int READ = 0;
 const int WRITE = 1;
 const int NUM_PIPES = 2;
+
 void close_pipes(int pipes[][2])
 {
-   int i;
-   for (i = 0; i < NUM_PIPES; i++)
-   {
-      close(pipes[i][READ]);
-      close(pipes[i][WRITE]);
-   }
+	int i;
+	for (i = 0; i < NUM_PIPES; i++)
+	{
+		close(pipes[i][READ]);
+		close(pipes[i][WRITE]);
+	}
 }
-int main() 
+
+int main()
 {
-   int pipes[NUM_PIPES][2];
-   int pid, status;
-   pipe(pipes[0]);
-   pipe(pipes[1]);
-   pid = fork();
-   if (pid == 0)
-   {
-      // Left child writes into WRITE end of first pipe:
-      dup2(pipes[0][WRITE], STDOUT_FILENO);
-      // Close each end of all pipes:
-      close_pipes(pipes);
-      // Read a string from stdin, print to stdout:
-      char str[100];
-      scanf("%s", str);
-      printf("%s", str);
-   }
-   else
-   {
-      pid = fork();
-      if (pid == 0)
-      {
-         // Middle child reads from READ end of first pipe
-         dup2(pipes[0][READ], STDIN_FILENO);
-         // Middle child writes to WRITE end of second pipe
-         dup2(pipes[1][WRITE], STDOUT_FILENO);
-         // Close each end of all pipes
-         close_pipes(pipes);
-         // Read a string from stdin, double the string to stdout:
-         char str[100];
-         scanf("%s", str);
-         printf("%s%s", str, str);
-      }
-      else
-      {
-         pid = fork();
-         if (pid == 0)
-         {
-            // Right child reads from READ end of second pipe:
-            dup2(pipes[1][READ], STDIN_FILENO);
-            // Close each end of all pipes 
-            close_pipes(pipes);
-            // Read a string from stdin, print to stdout:
-            char str[100];
-            scanf("%s", str);
-            printf("%s", str);
-         }
-         else
-         {
-            // Parent
-            // Close each end of all pipes
-            close_pipes(pipes);
-            // Wait until the last child terminates:
-            waitpid(pid, &status, 0);  // Or, maybe don't wait?
-         }
-      }
-   }
+	int pipes[NUM_PIPES][2];
+	int pid, status;
+	pipe(pipes[0]);
+	pipe(pipes[1]);
+	pid = fork();
+	if (pid == 0)
+	{
+		// Left child writes into WRITE end of first pipe:
+		dup2(pipes[0][WRITE], STDOUT_FILENO);
+		// Close each end of all pipes:
+		close_pipes(pipes);
+		// Read a string from stdin, print to stdout:
+		char str[100];
+		scanf("%s", str);
+		printf("%s", str);
+	}
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			// Middle child reads from READ end of first pipe
+			dup2(pipes[0][READ], STDIN_FILENO);
+			// Middle child writes to WRITE end of second pipe
+			dup2(pipes[1][WRITE], STDOUT_FILENO);
+			// Close each end of all pipes
+			close_pipes(pipes);
+			// Read a string from stdin, double the string to stdout:
+			char str[100];
+			scanf("%s", str);
+			printf("%s%s", str, str);
+		}
+		else
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				// Right child reads from READ end of second pipe:
+				dup2(pipes[1][READ], STDIN_FILENO);
+				// Close each end of all pipes
+				close_pipes(pipes);
+				// Read a string from stdin, print to stdout:
+				char str[100];
+				scanf("%s", str);
+				printf("%s", str);
+			}
+			else
+			{
+				// Parent
+				// Close each end of all pipes
+				close_pipes(pipes);
+				// Wait until the last child terminates:
+				waitpid(pid, &status, 0); // Or, maybe don't wait?
+			}
+		}
+	}
 }
